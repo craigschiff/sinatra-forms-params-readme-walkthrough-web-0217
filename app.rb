6e533fe8ad39dedec35d46fa3fad0d719dfa2527
github username <github email address>
2017-02-20 14:44:30 -0500

    done

--- app.rb
+++ app.rb
@@ -8,5 +8,14 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
+  post '/food' do
+  #  @fav_food = params[:favorite_food].to_s
+
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+#  post '/name' do
+#    @name = params[:name].to_s
+#  end
+
 
 end
